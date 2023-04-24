c176c9879a40cb5de2a3ec5bfca86e41255bd795
mina <higharcswastaken@gmail.com>
2023-04-23 22:04:34 -0500

    asdf

--- dist/index.js
+++ dist/index.js
@@ -6,38 +6,80 @@ const constants_1 = require("detritus-client/lib/constants");
 const matrix_bot_sdk_1 = require("matrix-bot-sdk");
 (async () => {
     const auth = new matrix_bot_sdk_1.MatrixAuth(process.env.homeserver);
-    const discordClient = new detritus_client_1.ShardClient(process.env.discord, { isBot: true, gateway: { intents: 'all' } });
+    const discordClient = new detritus_client_1.ShardClient(process.env.discord, { isBot: true, gateway: { intents: 3276799 } });
     const matrixClient = await auth.passwordLogin(process.env.matrix_user, process.env.matrix_pwd, 'bot');
     const channel = await discordClient.rest.fetchChannel(process.env.channel_id);
     await matrixClient.joinRoom(process.env.room_id);
     const marker = Date.now();
     const selfId = await matrixClient.getUserId();
+    let wh = undefined;
+    let last_sent = undefined;
+    let last_edited = undefined;
     matrixClient.on('room.event', async (roomId, event) => {
+        console.log(event);
         if (marker > event.origin_server_ts ||
             roomId !== process.env.room_id ||
             event.sender === selfId) {
             return;
         }
-        console.log(event);
-        await channel.createMessage(`[${event.sender}] ${event.content.body}`);
+        if (wh === undefined) {
+            const whs = await channel.fetchWebhooks();
+            const zwh = whs.find((v) => v.user?.id === discordClient.userId);
+            if (zwh !== undefined) {
+                wh = zwh;
+            }
+            else {
+                const nwh = await channel.createWebhook({
+                    name: event.sender
+                });
+                wh = nwh;
+            }
+        }
+        const profile = await matrixClient.getUserProfile(event.sender);
+        const dataUrl = profile.avatar_url;
+        const realUrl = (await matrixClient.downloadContent(dataUrl)).data;
+        if (last_sent !== event.sender) {
+            try {
+                await wh.edit({
+                    name: `${profile.displayname} (${event.sender})`,
+                    avatar: realUrl
+                });
+                last_edited = event.sender;
+            }
+            catch { }
+        }
+        last_sent = event.sender;
+        if (last_edited !== event.sender) {
+            await wh.createMessage({
+                content: `${profile.displayname} (${event.sender}):\n${event.content.body}`,
+                allowedMentions: { users: [], roles: [] }
+            });
+        }
+        else {
+            await wh.createMessage({
+                content: event.content.body,
+                allowedMentions: { users: [], roles: [] }
+            });
+        }
     });
     discordClient.on(constants_1.ClientEvents.MESSAGE_CREATE, async (payload) => {
-        console.log(payload.message.content);
         if (payload.message.fromWebhook ||
             payload.message.fromMe ||
             payload.message.channelId !== process.env.channel_id) {
             return void 0;
         }
-        void await matrixClient.sendMessage(process.env.room_id, {
-            body: `[${payload.message.author.tag}]: ${payload.message.content}`,
-            msgtype: 'm.text'
-        });
+        const uri = await matrixClient.uploadContentFromUrl(payload.message.author.avatarUrl);
+        await matrixClient.setAvatarUrl(uri);
+        await matrixClient.setDisplayName(payload.message.author.tag);
+        await matrixClient.sendText(process.env.room_id, payload.message.content);
     });
-    await discordClient.run();
+    await discordClient.run().then(() => {
+        console.log('ok! 1');
+    });
     matrixClient
         .start()
         .then(() => {
-        console.log('ok!');
+        console.log('ok! 2');
     })
         .catch(console.error);
 })();
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@
 import 'dotenv/config';
 
 import { ShardClient } from 'detritus-client';
+import type { BaseCollection } from 'detritus-client/lib/collections';
 import { ClientEvents } from 'detritus-client/lib/constants';
-import type { Channel } from 'detritus-client/lib/structures';
-import type { MatrixClient } from 'matrix-bot-sdk';
+import type { Channel, Webhook } from 'detritus-client/lib/structures';
+import type { MatrixClient, RoomEvent } from 'matrix-bot-sdk';
 import { MatrixAuth } from 'matrix-bot-sdk';
-
 (async (): Promise<void> => {
   const auth: MatrixAuth = new MatrixAuth(process.env.homeserver as string);
 
   const discordClient: ShardClient = new ShardClient(
     process.env.discord as string,
-    { isBot: true, gateway: { intents: 'all' } }
+    { isBot: true, gateway: { intents: 3276799 } }
   );
 
   const matrixClient: MatrixClient = await auth.passwordLogin(
@@ -31,25 +31,81 @@ import { MatrixAuth } from 'matrix-bot-sdk';
 
   const selfId: string = await matrixClient.getUserId();
 
-  matrixClient.on('room.event', async (roomId: string, event: unknown) => {
+  let wh: Webhook | undefined = undefined;
+  let last_sent: string | undefined = undefined;
+  let last_edited: string | undefined = undefined;
+  // let mc: MatrixClient | undefined = undefined;
+
+  // discord user => matrix user
+  // const userLinker: Map<string, MatrixClient> = new Map<string, MatrixClient>();
+
+  matrixClient.on('room.event', async (roomId: string, event: RoomEvent) => {
+    console.log(event);
     if (
       // @ts-expect-error(2023): nope
       marker > event.origin_server_ts ||
       roomId !== process.env.room_id ||
-      // @ts-expect-error(2023): nope
       event.sender === selfId
     ) {
       return;
     }
 
-    console.log(event);
+    // add editing later
+    // if (event.content['m.new_content'])
 
-    // @ts-expect-error(2023): nope
-    await channel.createMessage(`[${event.sender}] ${event.content.body}`);
+    // console.log(event);
+
+    if (wh === undefined) {
+      const whs: BaseCollection<string, Webhook> =
+        await channel.fetchWebhooks();
+
+      const zwh: Webhook | undefined = whs.find(
+        (v) => v.user?.id === discordClient.userId
+      );
+
+      if (zwh !== undefined) {
+        wh = zwh;
+      } else {
+        const nwh: Webhook = await channel.createWebhook({
+          name: event.sender
+        });
+
+        wh = nwh;
+      }
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+    const profile: any = await matrixClient.getUserProfile(event.sender);
+    const dataUrl: string = profile.avatar_url as string;
+
+    const realUrl: Buffer = (await matrixClient.downloadContent(dataUrl)).data;
+
+    if (last_sent !== event.sender) {
+      try {
+        await wh.edit({
+          name: `${profile.displayname} (${event.sender})`,
+          avatar: realUrl
+        });
+        last_edited = event.sender;
+      } catch {}
+    }
+
+    last_sent = event.sender;
+
+    if (last_edited !== event.sender) {
+      await wh.createMessage({
+        content: `${profile.displayname} (${event.sender}):\n${event.content.body}`,
+        allowedMentions: { users: [], roles: [] }
+      });
+    } else {
+      await wh.createMessage({
+        content: event.content.body,
+        allowedMentions: { users: [], roles: [] }
+      });
+    }
   });
 
   discordClient.on(ClientEvents.MESSAGE_CREATE, async (payload) => {
-    console.log(payload.message.content);
     if (
       payload.message.fromWebhook ||
       payload.message.fromMe ||
@@ -58,18 +114,26 @@ import { MatrixAuth } from 'matrix-bot-sdk';
       return void 0;
     }
 
-    void await matrixClient.sendMessage(process.env.room_id as string, {
-      body: `[${payload.message.author.tag}]: ${payload.message.content}`,
-      msgtype: 'm.text'
-    });
+    const uri: string = await matrixClient.uploadContentFromUrl(
+      payload.message.author.avatarUrl
+    );
+
+    await matrixClient.setAvatarUrl(uri);
+    await matrixClient.setDisplayName(payload.message.author.tag);
+    await matrixClient.sendText(
+      process.env.room_id as string,
+      payload.message.content
+    );
   });
 
-  await discordClient.run();
+  await discordClient.run().then(() => {
+    console.log('ok! 1');
+  });
 
   matrixClient
     .start()
     .then(() => {
-      console.log('ok!');
+      console.log('ok! 2');
     })
     .catch(console.error);
 })();
